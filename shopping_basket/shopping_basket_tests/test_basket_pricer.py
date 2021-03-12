2840e430296ab319247346ba6ebc04f04e2818dd
Michał Kosek <mihao@users.noreply.github.com>
2021-03-12 19:57:02 +0100

    Use pytest

--- shopping_basket/shopping_basket_tests/test_basket_pricer.py
+++ shopping_basket/shopping_basket_tests/test_basket_pricer.py
@@ -1,27 +1,8 @@
-import unittest
+import pytest
 from offer import Offer, BuyXgetY, PercentDiscount, BuyNofXGetCheapestForFree
 from basket import Basket
 from catalogue import Catalogue
 from basket_pricer import basket_pricer
-
-catalogue = Catalogue(
-    {
-        "Baked Beans": 0.99,
-        "Biscuits": 1.20,
-        "Sardines": 1.89,
-        "Shampoo (Small)": 2.00,
-        "Shampoo (Medium)": 2.50,
-        "Shampoo (Large)": 3.50,
-    }
-)
-
-offers = [
-    BuyXgetY("Baked Beans", 2, 1),
-    PercentDiscount("Sardines", 25),
-    BuyNofXGetCheapestForFree(
-        {"Shampoo (Large)", "Shampoo (Medium)", "Shampoo (Small)"}, 3
-    ),
-]
 
 
 class Broken1(Offer):
@@ -57,59 +38,85 @@ class Broken3(Offer):
         return -999
 
 
-class TestBasketPricer(unittest.TestCase):
-    def testBuyXgetY(self):
-        basket = Basket({"Baked Beans": 4, "Biscuits": 1})
-        self.assertEqual(basket_pricer(basket, catalogue, offers), (5.16, 0.99, 4.17))
-
-    def testBuyXgetYtwice(self):
-        basket = Basket({"Baked Beans": 6})
-        self.assertEqual(basket_pricer(basket, catalogue, offers), (5.94, 1.98, 3.96))
-
-    def testPercentDiscount(self):
-        basket = Basket({"Baked Beans": 2, "Biscuits": 1, "Sardines": 2})
-        self.assertEqual(basket_pricer(basket, catalogue, offers), (6.96, 0.95, 6.01))
-
-    def testBuyNofXGetCheapestForFree(self):
-        basket = Basket(
-            {"Shampoo (Large)": 3, "Shampoo (Medium)": 1, "Shampoo (Small)": 2}
-        )
-        self.assertEqual(basket_pricer(basket, catalogue, offers), (17.0, 5.5, 11.5))
-
-    def testBrokenOffersZeroDiscount(self):
-        basket = Basket({"Baked Beans": 1, "Biscuits": 2, "Sardines": 3})
-        self.assertEqual(
-            basket_pricer(basket, catalogue, [Broken1()]), (9.06, 0.0, 9.06)
-        )
-
-    def testBrokenOfferDiscountTooLarge(self):
-        basket = Basket({"Baked Beans": 1, "Biscuits": 2, "Sardines": 3})
-        with self.assertRaises(ValueError):
-            basket_pricer(basket, catalogue, [Broken2()])
-
-    def testBrokenOfferNegativeDiscount(self):
-        basket = Basket({"Baked Beans": 1, "Biscuits": 2, "Sardines": 3})
-        with self.assertRaises(ValueError):
-            basket_pricer(basket, catalogue, [Broken3()])
-
-    def testEmptyCatalogue(self):
-        basket = Basket({"item": 1})
-        with self.assertRaises(ValueError):
-            basket_pricer(basket, Catalogue({}), [Broken3()])
-
-    def testNegativePrice(self):
-        basket = Basket({"item": 1})
-        with self.assertRaises(ValueError):
-            basket_pricer(basket, Catalogue({"item": -1}), [Broken3()])
-
-    def testItemNotInCatalogue(self):
-        basket = Basket({"qwerty": 1})
-        with self.assertRaises(ValueError):
-            basket_pricer(basket, catalogue, offers)
-
-    def testEmptyBasket(self):
-        self.assertEqual(basket_pricer(Basket(), catalogue, offers), (0.0, 0.0, 0.0))
+@pytest.fixture
+def catalogue():
+    return Catalogue(
+        {
+            "Baked Beans": 0.99,
+            "Biscuits": 1.20,
+            "Sardines": 1.89,
+            "Shampoo (Small)": 2.00,
+            "Shampoo (Medium)": 2.50,
+            "Shampoo (Large)": 3.50,
+        }
+    )
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def offers():
+    return [
+        BuyXgetY("Baked Beans", 2, 1),
+        PercentDiscount("Sardines", 25),
+        BuyNofXGetCheapestForFree(
+            {"Shampoo (Large)", "Shampoo (Medium)", "Shampoo (Small)"}, 3
+        ),
+    ]
+
+
+def testBuyXgetY(catalogue, offers):
+    basket = Basket({"Baked Beans": 4, "Biscuits": 1})
+    assert basket_pricer(basket, catalogue, offers) == (5.16, 0.99, 4.17)
+
+
+def testBuyXgetYtwice(catalogue, offers):
+    basket = Basket({"Baked Beans": 6})
+    assert basket_pricer(basket, catalogue, offers) == (5.94, 1.98, 3.96)
+
+
+def testPercentDiscount(catalogue, offers):
+    basket = Basket({"Baked Beans": 2, "Biscuits": 1, "Sardines": 2})
+    assert basket_pricer(basket, catalogue, offers) == (6.96, 0.95, 6.01)
+
+
+def testBuyNofXGetCheapestForFree(catalogue, offers):
+    basket = Basket({"Shampoo (Large)": 3, "Shampoo (Medium)": 1, "Shampoo (Small)": 2})
+    assert basket_pricer(basket, catalogue, offers) == (17.0, 5.5, 11.5)
+
+
+def testBrokenOffersZeroDiscount(catalogue):
+    basket = Basket({"Baked Beans": 1, "Biscuits": 2, "Sardines": 3})
+    assert basket_pricer(basket, catalogue, [Broken1()]) == (9.06, 0.0, 9.06)
+
+
+def testBrokenOfferDiscountTooLarge(catalogue):
+    basket = Basket({"Baked Beans": 1, "Biscuits": 2, "Sardines": 3})
+    with pytest.raises(ValueError):
+        basket_pricer(basket, catalogue, [Broken2()])
+
+
+def testBrokenOfferNegativeDiscount(catalogue):
+    basket = Basket({"Baked Beans": 1, "Biscuits": 2, "Sardines": 3})
+    with pytest.raises(ValueError):
+        basket_pricer(basket, catalogue, [Broken3()])
+
+
+def testEmptyCatalogue():
+    basket = Basket({"item": 1})
+    with pytest.raises(ValueError):
+        basket_pricer(basket, Catalogue({}), [Broken3()])
+
+
+def testNegativePrice():
+    basket = Basket({"item": 1})
+    with pytest.raises(ValueError):
+        basket_pricer(basket, Catalogue({"item": -1}), [Broken3()])
+
+
+def testItemNotInCatalogue(catalogue, offers):
+    basket = Basket({"qwerty": 1})
+    with pytest.raises(ValueError):
+        basket_pricer(basket, catalogue, offers)
+
+
+def testEmptyBasket(catalogue, offers):
+    assert basket_pricer(Basket(), catalogue, offers) == (0.0, 0.0, 0.0)
--- shopping_basket/shopping_basket_tests/test_offer.py
+++ shopping_basket/shopping_basket_tests/test_offer.py
@@ -1,161 +1,154 @@
-import unittest
+import pytest
 from offer import Offer, BuyXgetY, PercentDiscount, BuyNofXGetCheapestForFree
 from basket import Basket
 from catalogue import Catalogue
 
 
-class TestOffer(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        self.buy_x_get_y = BuyXgetY("item", 3, 2)
-        self.percent_discount = PercentDiscount("item", 20)
-        self.cheapest_for_free_offer = BuyNofXGetCheapestForFree({"item1", "item2"}, 5)
-        super().__init__(*args, **kwargs)
-
-    def testOffer_is_applicable(self):
-        with self.assertRaises(NotImplementedError):
-            Offer().is_applicable(Basket({}))
-
-    def testOffer_select_items(self):
-        with self.assertRaises(NotImplementedError):
-            Offer().select_items(Basket({}))
-
-    def testOffer_products_per_offer(self):
-        self.assertEqual(Offer().products_per_offer(), None)
-
-    def testOffer_calculate_discount(self):
-        with self.assertRaises(NotImplementedError):
-            Offer().calculate_discount(Basket({}), Catalogue({}))
-
-    def testBuyXgetY_is_applicable_1(self):
-        self.assertEqual(self.buy_x_get_y.is_applicable(Basket({})), False)
-
-    def testBuyXgetY_is_applicable_2(self):
-        self.assertEqual(
-            self.buy_x_get_y.is_applicable(Basket({"otheritem": 5})), False
-        )
-
-    def testBuyXgetY_is_applicable_3(self):
-        self.assertEqual(self.buy_x_get_y.is_applicable(Basket({"item": 2})), False)
-
-    def testBuyXgetY_is_applicable_4(self):
-        self.assertEqual(
-            self.buy_x_get_y.is_applicable(Basket({"otheritem": 4, "item": 3})), False
-        )
-
-    def testBuyXgetY_is_applicable_5(self):
-        self.assertEqual(self.buy_x_get_y.is_applicable(Basket({"item": 4})), True)
-
-    def testBuyXgetY_select_items(self):
-        self.assertEqual(
-            self.buy_x_get_y.select_items(Basket({"item": 10})), Basket({"item": 5})
-        )
-
-    def testBuyXgetY_products_per_offer(self):
-        self.assertEqual(self.buy_x_get_y.products_per_offer(), None)
-
-    def testBuyXgetY_calculate_discount(self):
-        self.assertEqual(
-            self.buy_x_get_y.calculate_discount(
-                Basket({"item": 5}), Catalogue({"item": 2})
-            ),
-            4,
-        )
-
-    def testPercentDiscount_is_applicable_1(self):
-        self.assertEqual(self.percent_discount.is_applicable(Basket({})), False)
-
-    def testPercentDiscount_is_applicable_2(self):
-        self.assertEqual(
-            self.percent_discount.is_applicable(Basket({"otheritem": 5})), False
-        )
-
-    def testPercentDiscount_is_applicable_3(self):
-        self.assertEqual(self.percent_discount.is_applicable(Basket({"item": 1})), True)
-
-    def testPercentDiscount_is_applicable_4(self):
-        self.assertEqual(self.percent_discount.is_applicable(Basket({"item": 2})), True)
-
-    def testPercentDiscount_select_items(self):
-        self.assertEqual(
-            self.percent_discount.select_items(Basket({"item": 10})),
-            Basket({"item": 1}),
-        )
-
-    def testPercentDiscount_products_per_offer(self):
-        self.assertEqual(self.percent_discount.products_per_offer(), None)
-
-    def testPercentDiscount_calculate_discount(self):
-        self.assertEqual(
-            self.percent_discount.calculate_discount(
-                Basket({"item": 1}), Catalogue({"item": 10})
-            ),
-            2,
-        )
-
-    def testBuyNofXGetCheapestForFree_is_applicable_1(self):
-        self.assertEqual(self.cheapest_for_free_offer.is_applicable(Basket({})), False)
-
-    def testBuyNofXGetCheapestForFree_is_applicable_2(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.is_applicable(Basket({"item1": 4})), False
-        )
-
-    def testBuyNofXGetCheapestForFree_is_applicable_3(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.is_applicable(Basket({"item2": 4})), False
-        )
-
-    def testBuyNofXGetCheapestForFree_is_applicable_4(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.is_applicable(Basket({"item1": 5})), True
-        )
-
-    def testBuyNofXGetCheapestForFree_is_applicable_5(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.is_applicable(Basket({"item2": 5})), True
-        )
-
-    def testBuyNofXGetCheapestForFree_is_applicable_6(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.is_applicable(
-                Basket({"item1": 4, "item2": 1})
-            ),
-            True,
-        )
-
-    def testBuyNofXGetCheapestForFree_is_applicable_7(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.is_applicable(
-                Basket({"item1": 4, "item2": 2})
-            ),
-            True,
-        )
-
-    def testBuyNofXGetCheapestForFree_select_items(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.select_items(Basket({"item1": 4, "item2": 2})),
-            Basket({"item1": 5, "item2": 5}),
-        )
-
-    def testBuyNofXGetCheapestForFree_products_per_offer(self):
-        self.assertEqual(self.cheapest_for_free_offer.products_per_offer(), 5)
-
-    def testBuyNofXGetCheapestForFree_calculate_discount_1(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.calculate_discount(
-                Basket({"item1": 4, "item2": 1}), Catalogue({"item1": 10, "item2": 20})
-            ),
-            10,
-        )
-
-    def testBuyNofXGetCheapestForFree_calculate_discount_2(self):
-        self.assertEqual(
-            self.cheapest_for_free_offer.calculate_discount(
-                Basket({"item1": 4, "item2": 1}), Catalogue({"item1": 20, "item2": 5})
-            ),
-            5,
-        )
+@pytest.fixture
+def buy_x_get_y():
+    return BuyXgetY("item", 3, 2)
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def percent_discount():
+    return PercentDiscount("item", 20)
+
+
+@pytest.fixture
+def cheapest_for_free_offer():
+    return BuyNofXGetCheapestForFree({"item1", "item2"}, 5)
+
+
+def testOffer_is_applicable():
+    with pytest.raises(NotImplementedError):
+        Offer().is_applicable(Basket({}))
+
+
+def testOffer_select_items():
+    with pytest.raises(NotImplementedError):
+        Offer().select_items(Basket({}))
+
+
+def testOffer_products_per_offer():
+    assert Offer().products_per_offer() == None
+
+
+def testOffer_calculate_discount():
+    with pytest.raises(NotImplementedError):
+        Offer().calculate_discount(Basket({}), Catalogue({}))
+
+
+def testBuyXgetY_is_applicable_1(buy_x_get_y):
+    assert buy_x_get_y.is_applicable(Basket({})) == False
+
+
+def testBuyXgetY_is_applicable_2(buy_x_get_y):
+    assert buy_x_get_y.is_applicable(Basket({"otheritem": 5})) == False
+
+
+def testBuyXgetY_is_applicable_3(buy_x_get_y):
+    assert buy_x_get_y.is_applicable(Basket({"item": 2})) == False
+
+
+def testBuyXgetY_is_applicable_4(buy_x_get_y):
+    assert buy_x_get_y.is_applicable(Basket({"otheritem": 4, "item": 3})) == False
+
+
+def testBuyXgetY_is_applicable_5(buy_x_get_y):
+    assert buy_x_get_y.is_applicable(Basket({"item": 4})) == True
+
+
+def testBuyXgetY_select_items(buy_x_get_y):
+    assert buy_x_get_y.select_items(Basket({"item": 10})) == Basket({"item": 5})
+
+
+def testBuyXgetY_products_per_offer(buy_x_get_y):
+    assert buy_x_get_y.products_per_offer() == None
+
+
+def testBuyXgetY_calculate_discount(buy_x_get_y):
+    basket = Basket({"item": 5})
+    catalogue = Catalogue({"item": 2})
+    assert buy_x_get_y.calculate_discount(basket, catalogue) == 4
+
+
+def testPercentDiscount_is_applicable_1(percent_discount):
+    assert percent_discount.is_applicable(Basket({})) == False
+
+
+def testPercentDiscount_is_applicable_2(percent_discount):
+    assert percent_discount.is_applicable(Basket({"otheritem": 5})) == False
+
+
+def testPercentDiscount_is_applicable_3(percent_discount):
+    assert percent_discount.is_applicable(Basket({"item": 1})) == True
+
+
+def testPercentDiscount_is_applicable_4(percent_discount):
+    assert percent_discount.is_applicable(Basket({"item": 2})) == True
+
+
+def testPercentDiscount_select_items(percent_discount):
+    assert percent_discount.select_items(Basket({"item": 10})) == Basket({"item": 1})
+
+
+def testPercentDiscount_products_per_offer(percent_discount):
+    assert percent_discount.products_per_offer() == None
+
+
+def testPercentDiscount_calculate_discount(percent_discount):
+    basket = Basket({"item": 1})
+    catalogue = Catalogue({"item": 10})
+    assert percent_discount.calculate_discount(basket, catalogue) == 2
+
+
+def testBuyNofXGetCheapestForFree_is_applicable_1(cheapest_for_free_offer):
+    assert cheapest_for_free_offer.is_applicable(Basket({})) == False
+
+
+def testBuyNofXGetCheapestForFree_is_applicable_2(cheapest_for_free_offer):
+    assert cheapest_for_free_offer.is_applicable(Basket({"item1": 4})) == False
+
+
+def testBuyNofXGetCheapestForFree_is_applicable_3(cheapest_for_free_offer):
+    assert cheapest_for_free_offer.is_applicable(Basket({"item2": 4})) == False
+
+
+def testBuyNofXGetCheapestForFree_is_applicable_4(cheapest_for_free_offer):
+    assert cheapest_for_free_offer.is_applicable(Basket({"item1": 5})) == True
+
+
+def testBuyNofXGetCheapestForFree_is_applicable_5(cheapest_for_free_offer):
+    assert cheapest_for_free_offer.is_applicable(Basket({"item2": 5})) == True
+
+
+def testBuyNofXGetCheapestForFree_is_applicable_6(cheapest_for_free_offer):
+    basket = Basket({"item1": 4, "item2": 1})
+    assert cheapest_for_free_offer.is_applicable(basket) == True
+
+
+def testBuyNofXGetCheapestForFree_is_applicable_7(cheapest_for_free_offer):
+    basket = Basket({"item1": 4, "item2": 2})
+    assert cheapest_for_free_offer.is_applicable(basket) == True
+
+
+def testBuyNofXGetCheapestForFree_select_items(cheapest_for_free_offer):
+    basket_in = Basket({"item1": 4, "item2": 2})
+    basket_out = Basket({"item1": 5, "item2": 5})
+    assert cheapest_for_free_offer.select_items(basket_in) == basket_out
+
+
+def testBuyNofXGetCheapestForFree_products_per_offer(cheapest_for_free_offer):
+    assert cheapest_for_free_offer.products_per_offer() == 5
+
+
+def testBuyNofXGetCheapestForFree_calculate_discount_1(cheapest_for_free_offer):
+    basket = Basket({"item1": 4, "item2": 1})
+    catalogue = Catalogue({"item1": 10, "item2": 20})
+    assert cheapest_for_free_offer.calculate_discount(basket, catalogue) == 10
+
+
+def testBuyNofXGetCheapestForFree_calculate_discount_2(cheapest_for_free_offer):
+    basket = Basket({"item1": 4, "item2": 1})
+    catalogue = Catalogue({"item1": 20, "item2": 5})
+    assert cheapest_for_free_offer.calculate_discount(basket, catalogue) == 5
