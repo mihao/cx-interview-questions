8a1ae6990905125219648eea6f095556a5263bdd
Michał Kosek <mihao@users.noreply.github.com>
2021-03-12 14:13:24 +0100

    Refactoring and documentation

--- shopping_basket/basket_pricer.py
+++ shopping_basket/basket_pricer.py
@@ -5,40 +5,57 @@ from basket import Basket
 from catalogue import Catalogue
 
 
-def round_half_up(num, precision=2):
+def _round_half_up(num, precision=2):
     return float(
         (Decimal(str(num)) * Decimal(10) ** precision).quantize(1, ROUND_HALF_UP)
         / Decimal(10) ** precision
     )
 
 
-def basket_pricer(basket: Basket, prices: Catalogue, offers: List[Offer]):
-    def get_discount_and_offer_items(current_basket: Basket, offer: Offer):
-        if not offer.is_applicable(current_basket):
-            return None
-        offer_items = current_basket & offer.select_items(current_basket)
-        if not offer_items:
-            return None
-        products_per_offer = offer.products_per_offer()
-        if products_per_offer is not None:
-            sorted_items = sorted(
-                offer_items.elements(), key=lambda item: prices[item], reverse=True
-            )
-            offer_items = Basket(sorted_items[:products_per_offer])
-        discount = offer.calculate_discount(offer_items, prices)
-        if discount > sum(prices[item] for item in offer_items.elements()):
-            raise ValueError("discount is larger than the value of the items")
-        return (discount, offer_items)
+def _get_discount_and_offer_items(
+    current_basket: Basket, offer: Offer, prices: Catalogue
+):
+    """
+    Given a Basket and an Offer, the function returns None when the offer
+    is not applicable to the given basket, and otherwise returns a tuple
+    (discount, offer_items), where the first element is a float with the value
+    of the calculated discount, and the second element is a Basket of products
+    applicable to the discount.
+    """
 
-    try:
-        sub_total = sum(prices[item] for item in basket.elements())
-    except KeyError as e:
-        raise ValueError(f"basket item not found in the catalogue: {e}") from None
+    if not offer.is_applicable(current_basket):
+        return None
+    offer_items = current_basket & offer.select_items(current_basket)
+    if not offer_items:
+        return None
+    products_per_offer = offer.products_per_offer()
+    if products_per_offer is not None:
+        sorted_items = sorted(
+            offer_items.elements(), key=lambda item: prices[item], reverse=True
+        )
+        offer_items = Basket(sorted_items[:products_per_offer])
+    discount = offer.calculate_discount(offer_items, prices)
+    if discount > sum(prices[item] for item in offer_items.elements()):
+        raise ValueError("discount is larger than the value of the items")
+    return (discount, offer_items)
+
+
+def _calculate_discount_for_basket(
+    basket: Basket, offers: List[Offer], prices: Catalogue
+):
+    """
+    Main loop of the pricer algorithm. It starts with a temporary copy of the
+    whole basket, finds an offer that gives the biggest discount for the
+    given basket, and applies it as many times as possible, removing it from
+    the temporary basket. Then the whole process is repeated, until there is
+    no discount that can be applied to the remaining temporary basket.
+    """
+
     discount = 0.0
     undiscounted_basket_items = basket.copy()
     while True:
         offer_tuples = (
-            get_discount_and_offer_items(undiscounted_basket_items, offer)
+            _get_discount_and_offer_items(undiscounted_basket_items, offer, prices)
             for offer in offers
         )
         nonempty_offer_tuples = list(filter(None, offer_tuples))
@@ -52,7 +69,31 @@ def basket_pricer(basket: Basket, prices: Catalogue, offers: List[Offer]):
             discount_cnt += 1
             undiscounted_basket_items -= best_offer_items
         discount += discount_cnt * best_discount
+    return discount
 
-    sub_total_rounded = round_half_up(sub_total)
-    discount_rounded = round_half_up(discount)
-    return sub_total_rounded, discount_rounded, round_half_up(sub_total_rounded - discount_rounded)
+
+def basket_pricer(basket: Basket, prices: Catalogue, offers: List[Offer]):
+    """
+    Function for invoking the basket pricer.
+
+    Arguments:
+    basket: a Counter-like structure with item names as keys, and their quantity as the value
+    prices: a dict-like structure with item names as keys, and their prices as the value
+    offers: list of offers (subclasses of Offer, which is an abstract class)
+    Returned value:
+    tuple containing three floats: sub-total, discount and the total discounted price
+    """
+
+    try:
+        sub_total = sum(prices[item] for item in basket.elements())
+    except KeyError as e:
+        raise ValueError(f"basket item not found in the catalogue: {e}") from None
+
+    sub_total_rounded = _round_half_up(sub_total)
+    discount = _calculate_discount_for_basket(basket, offers, prices)
+    discount_rounded = _round_half_up(discount)
+    return (
+        sub_total_rounded,
+        discount_rounded,
+        _round_half_up(sub_total_rounded - discount_rounded),
+    )
--- shopping_basket/offer.py
+++ shopping_basket/offer.py
@@ -3,29 +3,37 @@ from catalogue import Catalogue
 
 
 class Offer:
-    # Should return True if the offer is applicable for the given basket, False
-    # otherwise.
     def is_applicable(self, basket: Basket) -> bool:
+        """
+        Should return True if the offer is applicable for the given basket, False
+        otherwise.
+        """
         raise NotImplementedError
 
-    # Should return a Basket with the items covered by the offer. Any elements
-    # that are not in the customer's basket will be ignored. The customer's
-    # basket is passed as an argument.
     def select_items(self, basket: Basket) -> Basket:
+        """
+        Should return a Basket with the items covered by the offer. Any elements
+        that are not in the customer's basket will be ignored. The customer's
+        basket is passed as an argument.
+        """
         raise NotImplementedError
 
-    # If the returned value is an int, it is the maximal number of products that
-    # can be covered by the offer. If the intersection of select_items() with
-    # the customer's basket has more than n elements, only n most expensive ones
-    # will be used (where n is the value returned by products_per_offer()). By
-    # default products_per_offer() returns None, which means that there is no
-    # limit.
     def products_per_offer(self) -> int:
+        """
+        If the returned value is an int, it is the maximal number of products that
+        can be covered by the offer. If the intersection of select_items() with
+        the customer's basket has more than n elements, only n most expensive ones
+        will be used (where n is the value returned by products_per_offer()). By
+        default products_per_offer() returns None, which means that there is no
+        limit.
+        """
         return None
 
-    # Given a Basket of items covered by the offer and a Catalogue of prices,
-    # calculate the value of the discount.
     def calculate_discount(self, offer_items: Basket, prices: Catalogue) -> float:
+        """
+        Given a Basket of items covered by the offer and a Catalogue of prices,
+        calculate the value of the discount.
+        """
         raise NotImplementedError
 
 
@@ -54,6 +62,8 @@ class PercentDiscount(Offer):
         return basket[self.item] >= 1
 
     def select_items(self, basket: Basket) -> Basket:
+        # All the discounts are applied iteratively, as long as it's possible.
+        # Therefore it's OK to select just one item for the discount here.
         return Basket({self.item: 1})
 
     def calculate_discount(self, offer_items: Basket, prices: Catalogue) -> float:
